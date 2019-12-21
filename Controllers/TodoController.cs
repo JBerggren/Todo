ceff1cef50d5b5579fb40002c3a35d99e212bfc8
Jonatan Berggren <jonatan.berggren@gmail.com>
2019-12-21 21:09:00 +0100

    Added compose file Added TodoController Removed obj/bin files

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoController : ControllerBase
+    {
+
+
+        private readonly ILogger<TodoController> _logger;
+
+        public TodoController(ILogger<TodoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
