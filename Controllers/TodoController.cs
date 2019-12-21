63b9e269295e478e900bf67bb26ee41eae5157f8
Jonatan Berggren <jonatan.berggren@gmail.com>
2019-12-21 21:23:35 +0100

    Added mongodb connection and it works!

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
 
 namespace Todo.Controllers
 {
@@ -23,7 +24,16 @@ namespace Todo.Controllers
         [HttpGet]
         public string Get()
         {
-            return "Hello world";
+            var mongo = new MongoClient("mongodb://todoadmin:getthingsdone@mongo:27017");
+            var db = mongo.GetDatabase("TodoDb");
+            var items = db.GetCollection<TodoItem>("items");
+            items.InsertOne(new TodoItem(){Title = DateTime.Now.ToString("mm:ss")});
+            return "Number of items " + items.CountDocuments(new FilterDefinitionBuilder<TodoItem>().Empty);
         }
+    }
+
+    public class TodoItem{
+        public string Title{get;set;}
+        public bool Done {get;set;}
     }
 }
--- dockerfile
+++ dockerfile
@@ -14,5 +14,4 @@ FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
 WORKDIR /app
 COPY --from=build-env /app/out .
 EXPOSE 80
-EXPOSE 443
 ENTRYPOINT ["dotnet", "Todo.dll"]
